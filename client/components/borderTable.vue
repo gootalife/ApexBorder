3229903dd3839bcc863e543bf64302e488afa84e
gootalife <gootalife0512@gmail.com>
2021-04-04 19:23:59 +0900

    add client side

---
+++ client/components/borderTable.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-simple-table dense>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th v-for="(header, key) in headers" :key="key">
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, key) in borderData" :key="key">
+          <td>{{ item.platform }}</td>
+          <td>{{ item.rp }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'nuxt-property-decorator';
+import * as border from '~/jsonFormat/borderFormat';
+import config from '../../apiserver/config.json';
+
+@Component
+export default class Index extends Vue {
+  @Prop({ default: [] })
+  private borderData!: border.Border[];
+  private headers = ['Platfrom', 'BorderRP'];
+  private platforms = Object.keys(config.platforms);
+}
+</script>
+
+<style scoped></style>
--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,10 +1,10 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from 'vuetify/es5/util/colors';
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - client',
-    title: 'client',
+    titleTemplate: '%s - ApexBorder',
+    title: 'ApexBorder',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -70,4 +70,4 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
   target: 'static',
-}
+};
